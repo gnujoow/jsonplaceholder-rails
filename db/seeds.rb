27e8ced612533d2691c6a88bd34a6ad0298db212
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-07-31 00:57:43 +0900

    show up Post

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'json'
+require 'open-uri'
+doc = JSON.parse(open('https://jsonplaceholder.typicode.com/posts').read)
+
+doc.each do |a|
+  Post.create(userId: a["userId"], title: a["title"], body: a["body"])
+end
